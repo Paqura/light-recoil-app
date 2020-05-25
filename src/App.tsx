6ee88f9b897603fc236611f25ce9b23519462f4b
Vyacheslav Popov <slavaavals616@gmail.com>
2020-05-25 19:18:13 +0400

    test basic of therecoil

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { atom, selector, useRecoilValue, useRecoilState } from 'recoil';
 
-function App() {
+export class Api {
+  constructor(private url: string) {}
+
+  async getComments() {
+    return await fetch(this.url).then(res => res.json())
+  }
+}
+
+const sinemaApi = new Api('https://jsonplaceholder.typicode.com/comments')
+
+const getFilms = selector({
+  key: 'comments',
+  get: async ({ get }: { get(...args: any[]): void }) => {
+    const response = await sinemaApi.getComments()
+    return response
+  },
+});
+
+const counterAtom = atom({
+  key: 'counter',
+  default: 0,
+})
+
+const Counter = () => {
+  const [counter, setCounter] = useRecoilState(counterAtom)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => setCounter(counter + 1)}>Click</button>
+      <p>{counter}</p>
+    </div>
+  )
+}
+
+interface Comment {
+  id: string;
+  body: string;
+}
+
+const App = () => {
+  const comments = useRecoilValue(getFilms) as Comment[]
+
+  return (
+    <div>
+      {comments.map(comment => (
+        <div key={comment.id}>
+          <div>{comment.body}</div>
+          <br />
+          <Counter />
+        </div>
+      ))}
     </div>
   );
 }
